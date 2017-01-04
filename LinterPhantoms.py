aa1c4b3f5d6c6ba47d3df9c026568dcdd7b113d2
herr kaste <herr.kaste@gmail.com>
2017-01-04 19:59:54 +0100

    Better LinterPhantoms

--- LinterPhantoms.py
+++ LinterPhantoms.py
@@ -1,6 +1,6 @@
 import sublime
 import sublime_plugin
-from SublimeLinter.lint import highlight, persist
+from SublimeLinter.lint import persist, highlight
 
 from collections import defaultdict
 
@@ -19,32 +19,43 @@ def PhantomSet(view):
         return set
 
 
+class ShowPhantomsCommand(sublime_plugin.EventListener):
+    def on_post_save_async(self, view):
+        if Active:
+            show_phantoms(view)
+
+    def on_modified_async(self, view):
+        if Active:
+            hide_phantoms(view)
+
+
 class ClearLinterPhantomsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         global Active
 
         view = self.view
-        phantom_set = PhantomSet(view)
         if view.id() not in Cleared:
-            phantom_set.update([])
-            Cleared.add(view.id())
+            hide_phantoms(view)
             Active = False
         else:
-            Active = not Active
-            if Active:
-                Cleared.remove(view.id())
-                show_phantoms(view, phantom_set, force=True)
+            show_phantoms(view)
+            Active = True
 
         sublime.status_message('LintPhantoms is ' +
                                ('active' if Active else 'inactive'))
 
 
 
+def hide_phantoms(view):
+    phantom_set = PhantomSet(view)
+    phantom_set.update([])
+    Cleared.add(view.id())
 
-def show_phantoms(view, phantom_set, force=False, margin=10):
+
+def show_phantoms(view, margin=0):
+    phantom_set = PhantomSet(view)
     vid = view.id()
     if vid in Cleared:
-        force = True
         Cleared.remove(vid)
 
     try:
@@ -52,7 +63,7 @@ def show_phantoms(view, phantom_set, force=False, margin=10):
     except KeyError:
         current_errors = []
 
-    needs_update = force is True or LastErrors[vid] != current_errors
+    needs_update = LastErrors[vid] != current_errors
 
     if needs_update:
         LastErrors[vid] = current_errors
@@ -62,7 +73,7 @@ def show_phantoms(view, phantom_set, force=False, margin=10):
         all_phantoms = Phantoms[vid]
 
     cr = current_row(view)
-    rg = range(cr - 5, cr + 15) if cr else []
+    rg = range(cr - margin, cr + margin) if cr else []
     phantoms = [phantom for row, phantom in all_phantoms.items()
                 if row not in rg]
     phantom_set.update(phantoms)
@@ -100,28 +111,19 @@ def show_above_below_markers(view, all_phantoms):
 
 
 def gen_phantoms(view, all_errors):
+    # type: (...) -> Dict[Row, Phantom]
+
     phantoms = {}
     for (row, errors) in all_errors.items():
         html = style_messages({error[1] for error in errors})
 
         line_start = view.text_point(row, 0)
-        prev_line_len = line_start - 1 - view.text_point(row - 1, 0)
-        if prev_line_len == 0:
-            region = sublime.Region(view.text_point(row - 1, 0))
-            layout = sublime.LAYOUT_BLOCK
-        else:
-            region = sublime.Region(
-                view.text_point(row - 1, min(20, prev_line_len)))
-            layout = sublime.LAYOUT_BELOW
+        prev_line_len = (line_start - 1) - view.text_point(row - 1, 0)
+        region = sublime.Region(
+            view.text_point(row - 1, min(20, prev_line_len)))
+        layout = sublime.LAYOUT_BELOW
 
-
-        if row == layout:
-            region = sublime.Region()
-
-        phantoms[row] = sublime.Phantom(
-            region,
-            html,
-            layout)
+        phantoms[row] = sublime.Phantom(region, html, layout)
 
     return phantoms
 
@@ -130,8 +132,9 @@ def gen_phantoms(view, all_errors):
 
 def style_messages(messages):
     html = ('<div style="'
-            'background-color: #e62d96; '
+            # 'background-color: #e62d96; '
             # 'background-color: #af3912; '
+            'background-color: #af1912; '
             'color: #fff; '
             'padding: 2px 4px">')
     for message in messages:
@@ -150,48 +153,4 @@ def current_row(view):
     selected_row, _ = view.rowcol(cursor.begin())
     return selected_row
 
-
-class ViewViewer(sublime_plugin.ViewEventListener):
-    def __init__(self, view):
-        self.view = view
-
-    def on_selection_modified(self):
-        if not Active:
-            return
-
-        sublime.set_timeout_async(
-            lambda: show_phantoms(self.view, PhantomSet(self.view), margin=10),
-            3000)
-
-
-class ViewCalculator(sublime_plugin.ViewEventListener):
-    def __init__(self, view):
-        self.view = view
-        self.timeout_scheduled = 0
-
-        self.update_phantoms()
-
-    def update_phantoms(self):
-        show_phantoms(self.view, PhantomSet(self.view))
-
-    def handler(self):
-        self.timeout_scheduled -= 1
-        if self.timeout_scheduled == 0:
-            self.update_phantoms()
-
-    def on_activated_async(self):
-        if not Active:
-            return
-
-        self.timeout_scheduled += 1
-        sublime.set_timeout(lambda: self.handler(), 1000)
-
-    def on_modified_async(self):
-        if not Active:
-            return
-
-        self.timeout_scheduled += 1
-        sublime.set_timeout(lambda: self.handler(), 2500)
-        # sublime.set_timeout(lambda: self.update_phantoms(), 10)
-        # self.update_phantoms()
 
