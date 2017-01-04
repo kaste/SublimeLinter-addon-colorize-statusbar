67533d7c9cad5fa03ce7e8f4534b1bca236f0c69
herr kaste <herr.kaste@gmail.com>
2017-01-04 20:55:37 +0100

    Show above/below markers

--- LinterPhantoms.py
+++ LinterPhantoms.py
@@ -1,8 +1,11 @@
 import sublime
 import sublime_plugin
-from SublimeLinter.lint import persist, highlight
+from SublimeLinter.lint import persist
 
 from collections import defaultdict
+
+
+LEFT_IDENT = 15  # try to indent the phantoms bc it looks better
 
 PhantomSets = {}
 Cleared = set()
@@ -70,43 +73,56 @@ def show_phantoms(view, margin=0):
     else:
         all_phantoms = Phantoms[vid]
 
+    # I had this; ah well... If you set a margin, phantoms around the current,
+    # edited line will not be drawn to reduce clutter.
     cr = current_row(view)
     rg = range(cr - margin, cr + margin) if cr else []
     phantoms = [phantom for row, phantom in all_phantoms.items()
                 if row not in rg]
     phantom_set.update(phantoms)
 
-    # show_above_below_markers(view, all_phantoms)
+    show_above_below_markers(view, all_phantoms)
 
 
 def show_above_below_markers(view, all_phantoms):
     visible_region = view.visible_region()
     first_row, _ = view.rowcol(visible_region.begin())
     last_row, _ = view.rowcol(visible_region.end())
-    print(first_row, last_row)
+
     above = False
     below = False
-    within = False
-    for row in all_phantoms.keys():
+    for row in sorted(all_phantoms.keys()):
         if row < first_row - 1:
             above = True
         elif row > last_row + 1:
+            if above:
+                # If we're here we're below the visible region, and since we
+                # only show above or below, we can break
+                break
             below = True
         elif first_row - 1 < row < last_row + 1:
-            within = True
+            # We already show phantoms within the visible region and don't want
+            # to distract even more
+            return
 
-    if not within:
-        if above:
-            print('above')
-            content = style_messages(['Errors above'])
-            view.show_popup(content, sublime.COOPERATE_WITH_AUTO_COMPLETE,
-                            max_width=600, location=visible_region.begin() + 1)
-        if below:
-            content = style_messages(['Errors below'])
-            view.show_popup(content, 0,
-                            max_width=600, location=visible_region.end())
+    # Although above and below could be True at the same time as a fact, we can
+    # only show one popup at any time. So we just shortcut here:
+    if above:
+        content = style_messages([center('Errors above')])
+        line_start = view.text_point(first_row, 0)
+        view.show_popup(content, sublime.COOPERATE_WITH_AUTO_COMPLETE,
+                        max_width=800, location=line_start)
+    if below:
+        content = style_messages([center('Errors below')])
+        line_start = view.text_point(last_row - 2, 0)
+        view.show_popup(content, sublime.COOPERATE_WITH_AUTO_COMPLETE,
+                        max_width=800, location=line_start)
 
 
+def center(text, cols=80, char='&nbsp;'):
+    fill_len = (cols - len(text)) // 2
+    fill = fill_len * char
+    return fill + text + fill
 
 def gen_phantoms(view, all_errors):
     # type: (...) -> Dict[Row, Phantom]
@@ -118,7 +134,7 @@ def gen_phantoms(view, all_errors):
         line_start = view.text_point(row, 0)
         prev_line_len = (line_start - 1) - view.text_point(row - 1, 0)
         region = sublime.Region(
-            view.text_point(row - 1, min(20, prev_line_len)))
+            view.text_point(row - 1, min(LEFT_IDENT, prev_line_len)))
         layout = sublime.LAYOUT_BELOW
 
         phantoms[row] = sublime.Phantom(region, html, layout)
@@ -150,5 +166,3 @@ def current_row(view):
 
     selected_row, _ = view.rowcol(cursor.begin())
     return selected_row
-
-
