d3a5e0fe9acb5cfa02069643ef644fed6536d9d7
herr kaste <herr.kaste@gmail.com>
2017-01-04 12:57:34 +0100

    First final shape.

--- show_tooltips.py
+++ show_tooltips.py
@@ -31,60 +31,51 @@ class ShowTooltipsSublimeLinterCommand(sublime_plugin.EventListener):
         sublime.set_timeout(lambda: self.maybe_show_tooltip(view), 1000)
 
 
-    def maybe_show_tooltip(self, view):
-        try:
-            html = self._compute_tooltip_html(view)
-        except (KeyError, IndexError):
-            self._last_html = ''
-            view.hide_popup()
-            return
-
-        # The tooltips disappear automatically, no need to do anything her
-        if not html:
-            return
-
-        if view.is_popup_visible():
-            view.update_popup(html)
-        else:
-            view.show_popup(html, sublime.COOPERATE_WITH_AUTO_COMPLETE,
-                            max_width=600, location=-1)
-
-
-    def _compute_tooltip_html(self, view):
+    def maybe_show_tooltip(self, view, force=False):
         # Get the line number of the first line of the first selection.
         row, col = current_pos(view)
 
         # on any vertical movement we force to see the tip
         if row != self._last_row:
             force = True
-
         self._last_row = row
 
-        all_errors = persist.errors[view.id()]
-        errors = all_errors[row]
+        errors = get_errors_on_line(view, row)
 
         # if we're really on the error force displaying
-        cols = [error[0] for error in errors]
-        if col in cols:
+        if any(c == col for c, m in errors):
             force = True
 
-        messages = [error[1] for error in errors]
-        html = get_html(messages)
-
-        if not force and html == self._last_html:
-            return
+        html = get_html(m for c, m in errors) if errors else ''
+        if force or html != self._last_html:
+            display_popup(view, html)
         self._last_html = html
 
-        # print('New message:', html)
 
-        return html
+def display_popup(view, html):
+    # The tooltips disappear automatically, no need to do anything here
+    if not html:
+        return
 
+    if view.is_popup_visible():
+        view.update_popup(html)
+    else:
+        view.show_popup(html, sublime.COOPERATE_WITH_AUTO_COMPLETE,
+                        max_width=600, location=-1)
+
+
+def get_errors_on_line(view, row):
+    try:
+        errors_by_view = persist.errors[view.id()]
+        return errors_by_view[row]
+    except (KeyError, IndexError):
+        return []
 
 def get_html(messages):
     return (
         STYLESHEET +
         '<div class="error">' +
-        ''.join('<span>' + m + '</span>' for m in messages) +
+        ''.join('<div>' + m + '</div>' for m in messages) +
         '</div>'
     )
 
